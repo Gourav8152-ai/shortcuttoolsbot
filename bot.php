28feb314533ae49bafdbf0bcadb05ed0a4aec085
Gourav8152-ai <goravsitbatch1@gmail.com>
2021-09-12 00:54:56 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,13 +8,13 @@ $id = $update["message"]["from"]["id"];
 $username = $update["message"]["from"]["username"];
 $firstname = $update["message"]["from"]["first_name"];
 $mention = "<a href='tg://user?id=$id'>$firstname</a>";
-$info ="<b>ID : </b>$id\n<b>First Name: </b>$firstname\n<b>Username : </b>@$username\n<b>Permanent Link : </b><a href='tg://user?id=$id'>$firstname</a>";
+$info ="ID : $id\nFirst Name: $firstname\nUsername : @$username\nPermanent Link : $mention";
 $info = urlencode($info);
 $welcome = $_ENV['WELCOME_MSG'];
 $inputcmd = "NOT YET ADDED";
 switch($message) {
     case "/start":
-        sendMessage($chatId, "<b>Type /cmd to know the command! </b>");
+        sendMessage($chatId, "Welcome To The Group $mention. \nUse /cmds to know the command.\n $welcome");
         break;
     case "/cmd":
         sendMessage($chatId,"This are the command: \n$inputcmd");
