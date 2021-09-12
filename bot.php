747ce1b5da81b9dcc93c6d6d3ccb6048a8a8ec6f
Gourav8152-ai <goravsitbatch1@gmail.com>
2021-09-13 01:03:16 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,20 +1,22 @@
 <?php
-$update = json_decode(file_get_contents('php://input'), TRUE);
-$message = $update["message"]["text"];
-$chatId = $update["message"]["chat"]["id"];
-$message_id = $update["message"]["message_id"];
-$id = $update["message"]["from"]["id"];
-$username = $update["message"]["from"]["username"];
-$firstname = $update["message"]["from"]["first_name"];
-// $mention = "["+$username+"](tg://user?id="+str(id)+")";
+$botToken = $_ENV['TOKEN'];
 $WELCOME_MSG = $_ENV['WELCOME_MSG'];
-
-if($message == "/start"){
-    send_message($chat_id,$message_id, "Welcome To The Group. \nUse /cmds to know the command.\n $WELCOME_MSG");
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+if (!$update) {
+    exit;
 }
+if (isset($update["message"])) {
+    $text = $update['message']['text'];
+    $chat_id = $update['message']['chat']['id'];
+    $message_id = $update['message']['message_id'];
 
-function send_message($chatId,$message_id, $message){
-    $apiToken = $_ENV['TOKEN'];
-    $text = urlencode($message);  
-    file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chatId&reply_to_message_id=$message_id&text=$text&parse_mode=HTML");
+
+    if (strpos($text, "/start") === 0) {
+        sendMessage($chat_id,$message_id, "Welcome To The Group. \nUse /cmds to know the command.\n $WELCOME_MSG");
+    }
+}
+function sendMessage($chat_id,$message_id, $message){
+    $text = urlencode($text); 
+    file_get_contents("https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chat_id&reply_to_message_id=$message_id&text=$text&parse_mode=HTML");
 }
