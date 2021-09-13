5b98a626fb71f6cb326b00960bad53e9a30d97f4
Gourav8152-ai <goravsitbatch1@gmail.com>
2021-09-13 11:34:41 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,10 +8,31 @@ if (!$update) {
     $message = $update['message']['text'];
     $chat_id = $update['message']['chat']['id'];
     $message_id = $update['message']['message_id'];
+    $user_id = $update['message']['from']['id'];
+    $first_name = $update['message']['from']['first_name'];
+    $username = $update['message']['from']['username'];
+    $r_user_id = $update['message']['reply_to_message']['from']['id'];
+    $r_first_name = $update['message']['reply_to_message']['from']['first_name'];
+    $r_username = $update['message']['reply_to_message']['from']['username'];
+    $r_message_id = $update['message']['reply_to_message']['message_id'];
     $WELCOME_MSG = $_ENV['WELCOME_MSG'];
 
+    if (!$r_user_id){
+        $r_user_id = $user_id;
+        $r_first_name = $first_name;
+        $r_username = $first_name;
+        $r_message_id = $message_id;
+    }
+
+
     if (strpos($message, "/start") === 0) {
-        sendMessage($chat_id,$message_id, "Welcome To The Group. \nUse /cmds to know the command.\n $WELCOME_MSG");
+        sendMessage($chat_id,$r_message_id, "Welcome To The Group. \nUse /cmds to know the command.\n $WELCOME_MSG");
+    }elseif (strpos($message, "/cmds") === 0) {
+        sendMessage($chat_id,$r_message_id, "Not yet added");
+    }elseif (strpos($message, "/info") === 0) {
+        sendMessage($chat_id,$r_message_id,"<b>ID : </b>$r_userId\n<b>First Name: </b>$r_firstname\n<b>Username : </b>@$r_username\n<b>Permanent Link : </b><a href='tg://user?id=$r_userId'>$r_firstname</a>");
+    }elseif (strpos($message, "/status") === 0) {
+        sendMessage($chat_id,$r_message_id,"ACTIVE");
     }
 }
 function sendMessage($chat_id,$message_id, $message){
